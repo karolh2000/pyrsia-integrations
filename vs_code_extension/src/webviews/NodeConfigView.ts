f573938a308a993c443627c983c95e9ba55087f9
Karol Harezlak <karolh@jfrog.com>
2022-12-16 19:32:04 -0800

    Minor refactoring

--- vs_code_extension/src/webviews/NodeConfigView.ts
+++ vs_code_extension/src/webviews/NodeConfigView.ts
@@ -8,18 +8,6 @@ export class NodeConfigView {
 		const view = vscode.window.createTreeView(NodeConfigView.VIEW_TYPE, { treeDataProvider: new NodeConfigTreeProvider(), showCollapseAll: true });
 
 		context.subscriptions.push(view);
-		// vscode.commands.registerCommand('testView.reveal', async () => {
-		// 	const key = await vscode.window.showInputBox({ placeHolder: 'Type the label of the item to reveal' });
-		// 	if (key) {
-		// 		await view.reveal({ key }, { focus: true, select: false, expand: true });
-		// 	}
-		// });
-		// vscode.commands.registerCommand('testView.changeTitle', async () => {
-		// 	const title = await vscode.window.showInputBox({ prompt: 'Type the new title for the Test View', placeHolder: view.title });
-		// 	if (title) {
-		// 		view.title = title;
-		// 	}
-		// });
 	}
 }
 
