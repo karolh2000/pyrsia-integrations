3dc818382d264a492ca5f14f78d3a6b98f870a49
Karol Harezlak <karolh@jfrog.com>
2022-12-16 16:51:30 -0800

    Tree view update

--- vs_code_extension/src/webviews/NodeConfigView.ts
+++ vs_code_extension/src/webviews/NodeConfigView.ts
@@ -5,7 +5,7 @@ export class NodeConfigView {
 	private static readonly VIEW_TYPE = "pyrsia.node-config";
 
 	constructor(context: vscode.ExtensionContext) {
-		const view = vscode.window.createTreeView(NodeConfigView.VIEW_TYPE, { treeDataProvider: aNodeWithIdTreeDataProvider(), showCollapseAll: true });
+		const view = vscode.window.createTreeView(NodeConfigView.VIEW_TYPE, { treeDataProvider: new NodeConfigTreeProvider(), showCollapseAll: true });
 
 		context.subscriptions.push(view);
 		// vscode.commands.registerCommand('testView.reveal', async () => {
@@ -25,85 +25,34 @@ export class NodeConfigView {
 
 class NodeConfigTreeProvider implements vscode.TreeDataProvider<string> {
 
+	private readonly nodeHostnameTreeItem = 
+		new NodeTreeItem('Node Hostname',
+						   vscode.TreeItemCollapsibleState.None,
+						   'node-hostanme');
 
-	getTreeItem: (element: { key: string }) => {
-		return null;
+	getTreeItem(element: string): vscode.TreeItem | Thenable<vscode.TreeItem>  {
+		let treeItem: NodeTreeItem;
+		switch (element) {
+			case this.nodeHostnameTreeItem.id: {
+				return this.nodeHostnameTreeItem;
+			}
+		}
+
+		throw new Error(`Element ${element} not found.`);
 	}
 
-
+	getChildren(element?: string | undefined): vscode.ProviderResult<string[]> {
+		return [this.nodeHostnameTreeItem.id];
+	}
 }
 
 class NodeTreeItem extends vscode.TreeItem {
-	constructor(lable: string, command: vscode.Command, tooltip: string) {
-		super(lable);
-		this.command = command;
-		this.tooltip = tooltip;
-	}
-}
-
-const tree: any = {
-	'Node Hostname: localhost': "localhost",
-	'Node Port: 7888': "7888"
-};
-const nodes: any = {};
-
-function aNodeWithIdTreeDataProvider(): vscode.TreeDataProvider<{ key: string }> {
-	return {
-		getChildren: (element: { key: string }): { key: string }[] => {
-			return getChildren(element ? element.key : undefined).map(key => getNode(key));
-		},
-		getTreeItem: (element: { key: string }): vscode.TreeItem => {
-			const treeItem = getTreeItem(element.key);
-			treeItem.id = element.key;
-			return treeItem;
-		},
-		getParent: ({ key }: { key: string }): { key: string } | undefined => {
-			const parentKey = key.substring(0, key.length - 1);
-			return parentKey ? new Key(parentKey) : undefined;
-		}
-	};
-}
-
-function getChildren(key: string | undefined): string[] {
-	if (!key) {
-		return Object.keys(tree);
-	}
-	const treeElement = getTreeElement(key);
-	if (treeElement) {
-		return Object.keys(treeElement);
-	}
-	return [];
-}
-
-function getTreeItem(key: string): vscode.TreeItem {
-	const treeElement = getTreeElement(key);
-	// An example of how to use codicons in a MarkdownString in a tree item tooltip.
-	const tooltip = new vscode.MarkdownString(`$(zap) Tooltip for ${key}`, true);
-	return {
-		label: /**vscode.TreeItemLabel**/<any>{ label: key, highlights: key.length > 1 ? [[key.length - 2, key.length - 1]] : void 0 },
-		tooltip,
-		collapsibleState: treeElement && Object.keys(treeElement).length ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
-	};
-}
-
-function getTreeElement(element: string): any {
-	let parent = tree;
-	for (let i = 0; i < element.length; i++) {
-		parent = parent[element.substring(0, i + 1)];
-		if (!parent) {
-			return null;
-		}
-	}
-	return parent;
-}
-
-function getNode(key: string): { key: string } {
-	if (!nodes[key]) {
-		nodes[key] = new Key(key);
-	}
-	return nodes[key];
-}
-
-class Key {
-	constructor(readonly key: string) { }
+	constructor(
+	  public readonly label: string,
+	  public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+	  public readonly id: string
+	) {
+	  super(label, collapsibleState);
+	  this.tooltip = this.label;
+	}  
 }
